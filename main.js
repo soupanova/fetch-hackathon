ab096226ab18cbb55ef3f1f5386da1f6214a510d
soupanova <duykhangkhuat@gmail.com>
2020-10-02 14:32:26 +0100

    timer

--- main.js
+++ main.js
@@ -7,39 +7,74 @@ async function trivia(){
     h2.innerText = data.results[0].question;
     return data.results[0].correct_answer;
 }
+const correctAnswer = trivia();
+
+
 let button = document.querySelector("button");
 button.addEventListener("click", trivia);
 
- 
-const correctAnswer = trivia();
-console.log(correctAnswer)
 
-// let questionsArray = []; 
+let input = document.querySelector("input");
 
-// questionsArray.push(allData);
+let p = document.querySelector("#timer")
 
 
-// let question = trivia(data.results[0-9].question)
+let count = 3;
 
-// let correctAnswer = data.result[0-9].correct_answer
+timer = setInterval(function() {
+    p.innerText = count--;
+    if (count === -1) {
+        clearInterval(timer);
+        return p.innerText = "Time's up"
+    } 
+}, 1000);
 
-let userinput = document.querySelector("input");
 
-// function correctAnswer(str){
-//     if (str === "anything"){
+
+
+
+
+
+
+// function timer(){
+//     let sec = 30;
+//     let timer = setInterval(function(){
+//         p.innerText="00" +sec;
+//         sec--;
+//         if (sec < 0) {
+//             clearInterval(timer);
+//         }
+//     }, 1000);
+// }
+
+// timer();
+
+
+
+
+// function Answer(string){
+//     let stringinput = input.innerText
+//     if (string === correctAnswer){
 //         return "Well done - thats correct"
 //     };
 // }
-
-
-// 10 question, once the fucntion pulls this data. 
-// we need to store the object/array which contains our questions
-// in storing this object we need to run the array so everytime the player clicks a button they can move on to the next question.
-
+// Answer()
 
 
 // let result = trivia()
 // console.log(result)
+
+
+
+//console.log(correctAnswer)
+// let questionsArray = []; 
+// questionsArray.push(allData);
+// let question = trivia(data.results[0-9].question)
+// let correctAnswer = data.result[0-9].correct_answer
+// 10 question, once the fucntion pulls this data. 
+// we need to store the object/array which contains our questions
+// in storing this object we need to run the array so everytime the player clicks a button they can move on to the next question.
+
 
 
 
